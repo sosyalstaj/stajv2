c99fb4e9afb67106441c2883090056739a5fa6dc
mustafazorbaz <mustafazorbaz@hotmail.com>
2016-05-19 20:33:14 +0300

    ilet

--- panel/hakkinda.php
+++ panel/hakkinda.php
@@ -1,10 +1,77 @@
 ﻿<?php
+global $conn;
+if(!$conn){
+	echo "veritabani bağlantı hatasi";
+}
+else{
+	
+	$sorgu="select * from tbl_hakkimizda where user_id=".$_SESSION["staj"]["id"];
+	if($sonuc=mysqli_query($conn,$sorgu))
+	{	
+		$array=mysqli_fetch_array($sonuc);
+	}
+		
+}
 
+if(@$_POST["kaydet"]){
+	global $conn;
+	$icerik=@$_POST["icerik"];
+	$tarih=@date('Y-m-d');
+	$userID=$_SESSION["staj"]["id"];
+	if(!mysqli_num_rows($sonuc)>0)
+	{
+		$sorgu2="INSERT INTO tbl_hakkimizda(user_id, baslik, icerik, tarih) VALUES ('$userID','xxx','$icerik','$tarih')";
+		if($sonuc=mysqli_query($conn,$sorgu2))
+		{	
+			echo '<div class="alert alert-success">
+				  <strong>Başarılı!</strong> İşlem başarılı bir şekilde kaydedildi...
+				</div>
+				<script>  alert("Başarılı...");
+				window.location="index.php?sayfa=hakkinda";
+		        </script> 
+				';
+		}
+		else{
+			echo '
+			<div class="alert alert-danger">
+			  <strong>Başarısız!</strong> Kaydetme işlemi başarısız.
+			</div>
+			<script>  alert("Başarısız!");
+				window.location="index.php?sayfa=hakkinda";
+		      </script> 
+			';
+		}
+	}
+	else{
+		$sorgu3="Update tbl_hakkimizda SET user_id='$userID', baslik='xxx', icerik='$icerik', tarih='$tarih' ";
+
+		if($sonuc=mysqli_query($conn,$sorgu3))
+		{	
+			echo '<div class="alert alert-info">
+				  <strong>Başarılı!</strong> İşlem başarılı bir şekilde güncellendi...
+				</div>
+				<script>  alert("Başarılı...");
+				window.location="index.php?sayfa=hakkinda";
+		        </script> 
+				';
+		}
+		else{
+			echo '<div class="alert alert-warning">
+				  <strong>Başarısız!</strong> Güncelleme gerçeklenemedi.
+				</div>
+				<script>  alert("Başarısız!");
+				window.location="index.php?sayfa=hakkinda";
+		      </script> 
+				';
+		}
+	}
+	
+}
 ?>
 
 <div class="row">
   <div >
-  <h3 class="baslik col-sm-12">Hakkinda</h3>
+  <h3 class="baslik col-sm-12">HAKKIMDA </h3>
   </div>
 </div>
 
@@ -15,39 +82,20 @@
        <span style="color:red; font-size:15px;" title="Bu alan zorunludur.">*</span>
       </label>
       <div class="col-sm-6">
-        <input type="text" class="form-control" id="icerik" placeholder="içerik"name="icerik"  >
+        <textarea class="form-control" rows="5" id="icerik" name="icerik"><?php echo @$array["icerik"];  ?></textarea>
 		</div>
 		</div>
 
         <div class="form-group">    
          <div class="row">
               <div class="col-sm-offset-2 col-sm-2">
-              <div class="form-group">
-			  <?php
+              <div class="form-group">				 
+      			<input type="submit" class="btn btn-default" name="kaydet" id="kaydet" value="KAYDET"/>
+				</div>
 			
-			global $conn;
-			$query1 ="select id from tbl_hakkimizda  where user_id ='".$_SESSION["staj"]["id"]."' ";
-			$kont=mysqli_query($conn,$query1);
-			$num_rows = mysqli_num_rows($kont);
-			if($num_rows==0)
-			{
-				echo'
-				 <input type="hidden" name="kaydet"  value="1"/>
-      			<button type="submit" class="btn btn-default" name="ekle" id="ekle">EKLE</button> 
-				</div>
-				';
-			}
-			else{
-				echo'
-				<input type="hidden" name="kaydet"  value="1"/>
-      			<button type="submit" class="btn btn-default" name="kaydet" id="kaydet">KAYDET</button> 
-				</div>
-				';
-			}
-			?>
-			 
                
               </div>
           </div>
 		</div>  
   </form>
+ 
