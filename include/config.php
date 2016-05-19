cafa8bccb5fa50bf199650b284ae2b58f764f620
Burak FIRAT <burakfiratz@gmail.com>
2016-05-19 13:12:24 +0300

    Revert "config footer"
    This reverts commit 26a00a3e82d25f7e928ec5ee703f530f6b257c97.

--- include/config.php
+++ include/config.php
@@ -6,6 +6,4 @@
 
 
 	$conn = mysqli_connect(host,user,pass,db) or die("Veri tabanı bağlanti hatası");
-	mysqli_set_charset($conn,"utf8");
-	mysqli_query($conn,"SET NAMES 'utf8' COLLATE 'utf8_turkish_ci'");
 ?>
