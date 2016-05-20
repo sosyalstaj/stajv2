4ed42f9eba9ddd7d0206fd9997ae0f8501140bfc
Burak FIRAT <burakfiratz@gmail.com>
2016-05-20 04:35:47 +0300

    aka sol menu v5 bf

--- panel/akademisyenSolMenu.php
+++ panel/akademisyenSolMenu.php
@@ -8,34 +8,27 @@
             <li><a href="index.php?sayfa=profil-duzenle">Düzenle</a></li>
           </ul>
         </li>
-        <li><a href="index.php?sayfa=mesajlar"><i class="fa fa-link"></i> <span>MESAJLAR</span></a></li>
+        <li><a href="index.php?sayfa=mesajlar"><i class="fa fa-envelope"></i> <span>MESAJLAR</span></a></li>
        <li class="treeview">
-          <a href="#"><i class="fa fa-link"></i> <span>ÖĞRENCİ İŞLEM</span> <i class="fa fa-angle-left pull-right"></i></a>
+          <a href="#"><i class="fa fa-laptop"></i> <span>ÖĞRENCİ İŞLEM</span> <i class="fa fa-angle-left pull-right"></i></a>
           <ul class="treeview-menu">
             <li><a href="index.php?sayfa=ogrenci-goster">Onay Bekleyen</a></li>
             <li><a href="index.php?sayfa=ogrenci-listem">Öğrenci Listesi</a></li>
           </ul>
         </li>
 		<li class="treeview">
-          <a href=""><i class="fa fa-link"></i> <span>DUYURULAR</span> <i class="fa fa-angle-left pull-right"></i></a>
+          <a href=""><i class="fa fa-calendar"></i> <span>DUYURULAR</span> <i class="fa fa-angle-left pull-right"></i></a>
           <ul class="treeview-menu">
             <li><a href="index.php?sayfa=duyurular-goster">Göster</a></li>
             <li><a href="index.php?sayfa=duyurular-ekle">Ekle</a></li>
           </ul>
         </li>
 		<li class="treeview">
-          <a href="#"><i class="fa fa-link"></i> <span>ETKİNLİKLER</span> <i class="fa fa-angle-left pull-right"></i></a>
-          <ul class="treeview-menu">
-            <li><a href="index.php?sayfa=etkinlikler-goster">Göster</a></li>
-            <li><a href="index.php?sayfa=etkinlikler-ekle">Ekle</a></li>
-          </ul>
-        </li>
-					<li class="treeview">
-          <a href="#"><i class="fa fa-link"></i> <span>STAJ EŞLEŞMELERİ</span> <i class="fa fa-angle-left pull-right"></i></a>
+          <a href="#"><i class="fa fa-files-o"></i> <span>STAJ EŞLEŞMELERİ</span> <i class="fa fa-angle-left pull-right"></i></a>
           <ul class="treeview-menu">
             <li><a href="index.php?sayfa=staj_sonuclananlar">Sonuçlananlar</a></li>
             <li><a href="index.php?sayfa=staj_beklenenler">Bekleyenler</a></li>
           </ul>
         </li>
-		  <li><a href="index.php?sayfa=iletisim"><i class="fa fa-link"></i> <span>İLETİŞİM</span></a></li>
+		  <li><a href="index.php?sayfa=iletisim"><i class="fa fa-share"></i> <span>İLETİŞİM</span></a></li>
       </ul>
